9701e9f9e777c6bd18b5bad436464557f89f5175
DatZ2H <quocdatdqd.cdt@gmail.com>
2023-06-03 18:53:22 +0700

    thêm tính năng safety theo vùng

--- src/obstacle_detection_node.cpp
+++ src/obstacle_detection_node.cpp
@@ -9,6 +9,8 @@
 #include <pcl/search/kdtree.h>
 #include <pcl/point_types.h>
 #include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/common/centroid.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <tf2/transform_datatypes.h>
@@ -38,13 +40,14 @@ public:
         "filtered_point_cloud_topic", 10);
     downsampled_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
         "downsampled_point_cloud_topic", 10);
-    sor_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+    denoised_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
         "StatisticalOutlierRemoval_point_cloud_topic", 10);
     obstacles_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
         "obstacles_point_cloud_topic", 10);
     clustered_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
         "clustered_point_cloud_topic", 10);
     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+
   }
 
 private:
@@ -82,20 +85,19 @@ private:
     downsampled_publisher_->publish(downsampled_msg);
 
     // Apply StatisticalOutlierRemoval filter to remove outliers
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_sor(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_denoised(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
     sor.setInputCloud(cloud_downsampled);
     sor.setMeanK(50);            // Number of nearest neighbors to compute mean distance
     sor.setStddevMulThresh(1.0); // Standard deviation multiplier threshold
-    sor.filter(*cloud_sor);
+    sor.filter(*cloud_denoised);
 
     // Publish filtered point cloud
-    sensor_msgs::msg::PointCloud2 sor_msg;
-    pcl::toROSMsg(*cloud_sor, sor_msg);
-    sor_publisher_->publish(sor_msg);
+    sensor_msgs::msg::PointCloud2 denoised_msg;
+    pcl::toROSMsg(*cloud_denoised, denoised_msg);
+    denoised_publisher_->publish(denoised_msg);
 
     // Step 3: RANSAC segmentation to separate ground plane from obstacles
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_obstacles(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
     pcl::SACSegmentation<pcl::PointXYZ> seg;
@@ -107,10 +109,16 @@ private:
     seg.setDistanceThreshold(0.01); // Adjust the distance threshold as needed
     seg.segment(*inliers, *coefficients);
 
-    // Extract the obstacles by removing the ground plane
+    // Extract ground plane
+    pcl::PointCloud<pcl::PointXYZ>::Ptr ground_plane(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::ExtractIndices<pcl::PointXYZ> extract;
     extract.setInputCloud(cloud_downsampled);
     extract.setIndices(inliers);
+    extract.setNegative(false);
+    extract.filter(*ground_plane);
+
+    // Remove ground plane from the point cloud
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_obstacles(new pcl::PointCloud<pcl::PointXYZ>);
     extract.setNegative(true);
     extract.filter(*cloud_obstacles);
 
@@ -122,47 +130,97 @@ private:
 
     // Step 4: Euclidean clustering to group obstacles
     std::vector<pcl::PointIndices> cluster_indices;
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
-    tree->setInputCloud(cloud_obstacles);
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr kd_tree(new pcl::search::KdTree<pcl::PointXYZ>);
+    kd_tree->setInputCloud(cloud_obstacles);
     pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
     ec.setClusterTolerance(0.02); // Adjust the cluster tolerance as needed
     ec.setMinClusterSize(50);    // Adjust the minimum cluster size as needed
     ec.setMaxClusterSize(10000);  // Adjust the maximum cluster size as needed
-    ec.setSearchMethod(tree);
+    ec.setSearchMethod(kd_tree);
     ec.setInputCloud(cloud_obstacles);
     ec.extract(cluster_indices);
 
     // Publish the clustered point cloud
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_clustered(new pcl::PointCloud<pcl::PointXYZRGB>);
-    for (const auto &indices : cluster_indices)
+    // Iterate through each cluster
+    for (const auto& indices : cluster_indices)
     {
-      for (const auto &index : indices.indices)
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered(new pcl::PointCloud<pcl::PointXYZ>);
+      for (const auto& index : indices.indices)
       {
-        pcl::PointXYZRGB point;
-        point.x = cloud_obstacles->points[index].x;
-        point.y = cloud_obstacles->points[index].y;
-        point.z = cloud_obstacles->points[index].z;
-        point.r = 255;
-        point.g = 0;
-        point.b = 0;
-        cloud_clustered->points.push_back(point);
+        cloud_clustered->points.push_back(cloud_obstacles->points[index]);
       }
-    }
-    cloud_clustered->width = cloud_clustered->points.size();
-    cloud_clustered->height = 1;
-    cloud_clustered->is_dense = true;
+      cloud_clustered->width = cloud_clustered->points.size();
+      cloud_clustered->height = 1;
+      cloud_clustered->is_dense = true;
 
     sensor_msgs::msg::PointCloud2 clustered_msg;
     pcl::toROSMsg(*cloud_clustered, clustered_msg);
     clustered_msg.header = msg->header;
     clustered_publisher_->publish(clustered_msg);
 
-    // Publish transform from camera_link to base_link
-    pcl::PointXYZ camera_translation(0.0, 0.0, 0.2); // Modify the camera translation according to the camera position
-    tf2::Quaternion camera_rotation;
-    camera_rotation.setRPY(-1.5708, 0, 0); // Modify the rotation angles according to the camera orientation
-    publishTransform("base_link", "camera_link", camera_translation, camera_rotation);
+        // Set the initial transformation from camera_link to base_link
+    pcl::PointXYZ translation(0.0, 0.0, 0.2);
+    tf2::Quaternion rotation;
+    rotation.setRPY(-1.5708, 0, 0);
+    publishTransform("base_link", "camera_link", translation, rotation);
+
+    // Perform additional processing to detect motion in warning and protection zones
+      detectMotionInZones(cloud_clustered);
   }
+  }
+  void detectMotionInZones(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud_clustered)
+  {
+    // Define the warning zone dimensions and distance thresholds
+    double warning_zone_x_min = -0.5;    // Modify the warning zone x min value as needed
+    double warning_zone_x_max = 0.5;     // Modify the warning zone x max value as needed
+    double warning_zone_y_min = -0.1;    // Modify the warning zone y min value as needed
+    double warning_zone_y_max = 0.1;     // Modify the warning zone y max value as needed
+    double warning_distance_threshold = 0.5;   // Modify the warning distance threshold as needed
+
+    // Define the protection zone dimensions and distance thresholds
+    double protection_zone_x_min = -0.25;    // Modify the protection zone x min value as needed
+    double protection_zone_x_max = 0.25;     // Modify the protection zone x max value as needed
+    double protection_zone_y_min = -0.1;    // Modify the protection zone y min value as needed
+    double protection_zone_y_max = 0.1;     // Modify the protection zone y max value as needed
+    double protection_distance_threshold = 0.3;   // Modify the protection distance threshold as needed
+
+    // Calculate the centroid of the cluster
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid(*cloud_clustered, centroid);
+
+    // Check if the centroid is within the warning zone
+    if (centroid[0] >= warning_zone_x_min && centroid[0] <= warning_zone_x_max &&
+        centroid[1] >= warning_zone_y_min && centroid[1] <= warning_zone_y_max)
+    {
+      // Calculate the distance from the centroid to the origin (base_link)
+      double distance = std::sqrt(centroid[0] * centroid[0] + centroid[1] * centroid[1]);
+
+      // Check if the distance is less than the warning distance threshold
+      if (distance <= warning_distance_threshold)
+      {
+        // Publish a warning message
+        RCLCPP_WARN(this->get_logger(), "Object detected in warning zone");
+        // Perform additional actions as needed
+      }
+    }
+
+    // Check if the centroid is within the protection zone
+    if (centroid[0] >= protection_zone_x_min && centroid[0] <= protection_zone_x_max &&
+        centroid[1] >= protection_zone_y_min && centroid[1] <= protection_zone_y_max)
+    {
+      // Calculate the distance from the centroid to the origin (base_link)
+      double distance = std::sqrt(centroid[0] * centroid[0] + centroid[1] * centroid[1]);
+
+      // Check if the distance is less than the protection distance threshold
+      if (distance <= protection_distance_threshold)
+      {
+        // Publish an error message
+        RCLCPP_ERROR(this->get_logger(), "Object detected in protection zone! Emergency stop!");
+        // Perform additional actions as needed
+      }
+    }
+  }
+
   void publishTransform(const std::string &frame_id, const std::string &child_frame_id,
                         const pcl::PointXYZ &translation, const tf2::Quaternion &rotation)
   {
@@ -184,7 +242,7 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr filtered_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr downsampled_publisher_;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr sor_publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr denoised_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr obstacles_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr clustered_publisher_;
 
